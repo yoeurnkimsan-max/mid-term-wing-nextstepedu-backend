82e619a7f00f6b1d0cc56c625dfa135fd080ffca
chory-chanrady-wu <chorychanrady.wu@gmail.com>
2026-02-15 17:52:41 +0700

    add route find profile by id

--- src/main/java/com/NextStepEdu/repositories/UserProfileRepository.java
+++ src/main/java/com/NextStepEdu/repositories/UserProfileRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UserProfileRepository  extends JpaRepository<UserProfileModel , Integer> {
+public interface UserProfileRepository extends JpaRepository<UserProfileModel, Integer> {
     boolean existsByUser(UserModel user);
+
     @Query("SELECT p FROM UserProfileModel p JOIN FETCH p.user")
     List<UserProfileModel> findAllWithUser();
+
+    @Query("SELECT p FROM UserProfileModel p JOIN FETCH p.user WHERE p.user.id = :userId")
+    Optional<UserProfileModel> findByUserId(Integer userId);
 }
